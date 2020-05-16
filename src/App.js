698564d634263c539fa3c568f761cefb65d3b7b7
Kevin Castillo <kevincastillo318@gmailcom>
2020-05-16 12:53:08 -0400

    update for easier start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ycth from './img/ycth.png'
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Boilerplate</h2>
         </div>
-        <p className="App-intro bg-yellow">
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+    <img src={ycth} className="ycth"/>
       </div>
     );
   }
