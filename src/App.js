a36f6e518248495000f49be15a65eb5e60fc01c4
Kevin Castillo <annefrankenstein@Kevins-MacBook-Air.local>
2020-12-06 18:21:24 -0500

    updated img alt tag

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-    <img src={ycth} className="ycth"/>
+    <img src={ycth} className="ycth" alt="ycth img"/>
       </div>
     );
   }
