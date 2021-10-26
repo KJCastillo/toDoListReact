ba461696166495d5f3ffe6d51e1d12a722a74434
Kevin Castillo <kevincastillo318@gmail.com>
2021-10-26 12:10:51 -0700

    form created

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
-import ycth from "../img/ycth.png";
+
 import "../App.css";
 
 class Body extends Component {
   render() {
     return (
-      <div className="body">
-        <p className="App-intro pt-5">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <img src={ycth} className="ycth" alt="ycth img" />
+      <div classNameName="body">
+        <form className="form-inline">
+          <div className="form-group mx-sm-3 mb-2">
+            <label for="input" className="sr-only">
+              Task
+            </label>
+            <input
+              type="input"
+              className="form-control"
+              id="input2"
+              placeholder="Task"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mb-2">
+            Add
+          </button>
+        </form>
       </div>
     );
   }
